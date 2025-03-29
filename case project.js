8106010c8fb841e8dda06276f6b80efaec618976
Euly Casias <epcasias@amarillocollege.com>
2025-03-29 12:05:31 -0500

    first commit

---
+++ case project.js
@@ -0,0 +1,16 @@
+//CASE PROJECT #1: Adding Else if statement to Pages
+//Author: Eulyssia casias
+//Date: 3/29/25
+
+//Add event handler to an element
+//use addEventListener to add windowalert when nav link is clicked
+document.addEventListener('click', function(event) {
+    //create else if statement targeting the event ID
+    if (event.target.id === "catGallery") {
+        window.alert("View the Cat Gallery"); //execute when home navigation is clicked
+    } else if (event.target.id === "descriptions") {
+        window.alert("Lets Go Into Detail!"); //execute when description navigation is clicked
+    } else if (event.target.id === "contact") {
+        window.alert("View our Socials!") //execute when contact navigation is clicked
+    }
+})
